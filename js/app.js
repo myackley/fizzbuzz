1fa94c5b308d2d55f2114943759a43c622ddbc69
Mark Yackley <myackley@hotmail.com>
2014-11-28 16:47:56 -0600

    Refactored code into a function for the loop, a parsed user-entered number, and input validation

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 $(document).ready(function() {
 
-	//establish for loop from 1 to 100
-	for (var i = 1; i < 101; i++) {
-		if (i%3==0 && i%5==0) {
-			//if divisible by 3 and 5 print "fizz buzz"
-			$("body").append("fizzbuzz\n");
-		} else if (i%3==0) {
-			//if divisible by only 3 "fizz"
-			$("body").append("fizz\n");
-		} else if (i%5==0) {
-			//if divisible by only 5 "buzz"
-			$("body").append("buzz\n");
-		} else {
-			//if not divisible by 3 or 5 list the number
-			$("body").append(i + "\n");
+	//user input parsed
+	var number = parseFloat(prompt("Please enter a number"));
+
+	//function
+	var toNum = function(number) {
+		//loop the number
+		for (var i = 1; i <= number; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				//if divisible by 3 and 5 print "fizz buzz"
+				$("body").append("fizzbuzz\n");
+			} else if (i % 3 == 0) {
+				//if divisible by only 3 print "fizz"
+				$("body").append("fizz\n");
+			} else if (i % 5 == 0) {
+				//if divisible by only 5 print "buzz"
+				$("body").append("buzz\n");
+			} else {
+				//print i if not divisible
+				$("body").append(i + "\n");
+			}
 		}
-	//end for loop 
+	};
+
+	//validate input
+	if (number % 1 != 0) {
+		alert("Please enter a whole number");
+	} else if (number < 0) {
+		alert("Please enter a positive number");
+	} else if (number == NaN) {
+		alert("Please enter a valid number");
+	} else {
+		toNum(number);
 	}
 	
+	$("#newnum").on("click", function() {
+		location.reload();
+	})
+
 });
